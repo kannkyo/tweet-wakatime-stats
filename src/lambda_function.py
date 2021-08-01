8c22da0a7cd621a485e4f0fff9f0e4d57b5f8a87
kannkyo <15080890+kannkyo@users.noreply.github.com>
2021-08-01 19:04:49 +0900

    Update envvalue

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -49,10 +49,10 @@ def lambda_handler(event, context):
         logger.info(secret)
 
         twitter = OAuth1Session(
-            secret['api_key'],
-            secret['api_secret_key'],
-            secret['access_token'],
-            secret['access_token_secret']
+            secret[os.environ.get('API_KEY')],
+            secret[os.environ.get('API_SECRET_KEY')],
+            secret[os.environ.get('ACCESS_TOKEN')],
+            secret[os.environ.get('ACCESS_TOKEN_SECRET')]
         )
 
         media_id = tweet_wakatime(
