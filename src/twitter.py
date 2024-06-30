5964eedad4b2e64473cf9b74e225e22e9fb90820
kannkyo <15080890+kannkyo@users.noreply.github.com>
2024-06-30 22:46:42 +0900

    twitter api 2.0 support

--- src/twitter.py
+++ src/twitter.py
@@ -12,16 +12,18 @@ def tweet_image(twitter: OAuth1Session, img_url: str):
     data = response.read()
     files = {"media": data}
     res_media = twitter.post(url_media, files=files)
-    logger.info(res_media)
+    logger.info(res_media.json())
 
     return res_media
 
 
 def tweet_text(twitter: OAuth1Session, message: str, media_ids: str = None):
-    url_text = "https://api.twitter.com/1.1/statuses/update.json"
+    url_text = "https://api.twitter.com/2/tweets"
 
-    params = {'status': message, "media_ids": media_ids}
-    res_text = twitter.post(url_text, params=params)
-    logger.info(res_text)
+    body = {'text': message}
+    if media_ids:
+        body["media"] = {"media_ids": media_ids}
+    res_text = twitter.post(url_text, json=body)
+    logger.info(res_text.json())
 
     return res_text
